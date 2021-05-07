14d55b1e2871e37cabfe80e2bbeb0bd02ab38fac
willian95 <rodriguezwillian95@gmail.com>
2021-05-07 05:40:29 -0400

    cambios

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('event:notifications')->dailyAt('9:40');
+        $schedule->command('event:notifications')->dailyAt('9:42');
     }
 
     /**
