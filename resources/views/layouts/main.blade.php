aa5cc6699400b8f6f064b69a1f31a8a409590815
willian95 <rodriguezwillian95@gmail.com>
2021-05-30 11:10:21 -0400

    cambios

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	<!--begin::Head-->
 	<head><base href="../../../">
 		<meta charset="utf-8" />
-		<title>JMP CMS</title>
+		<title>FIAN CMS</title>
 		<meta name="description" content="User datatable listing" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<link rel="canonical" href="https://keenthemes.com/metronic" />
